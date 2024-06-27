68650ab9c57f708103f1ba6ed642a631a513afb9
Samet Candan <c._samet@hotmail.com>
2024-06-27 23:58:00 +0300

    cal form

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -8,43 +8,48 @@ const Header = () => {
   return (
     <>
       <Navbar expand="lg" className=" safety-yellow-back">
-        <Container>
-          <Navbar.Brand>
-            <Nav.Link as={Link} to="/">
-              <img className="logo-img" src="loadcal_logo.png" alt="" />
-            </Nav.Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/containers">
-                Containers
+        <Container className="d-flex justify-content-between ">
+          <div className="d-flex align-items-center">
+            <Navbar.Brand>
+              <Nav.Link as={Link} to="/">
+                <img className="logo-img" src="loadcal_logo.png" alt="" />
               </Nav.Link>
-              <Nav.Link as={Link} to="/boxes">
-                Boxes
-              </Nav.Link>
-              <Nav.Link as={Link} to="/plans">
-                Plans
-              </Nav.Link>
-              <Nav.Link as={Link} to="/calculate">
-                Calculate
-              </Nav.Link>
+            </Navbar.Brand>
+          </div>
+          <div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/containers">
+                  Containers
+                </Nav.Link>
+                <Nav.Link as={Link} to="/boxes">
+                  Boxes
+                </Nav.Link>
+                <Nav.Link as={Link} to="/plans">
+                  Plans
+                </Nav.Link>
+                <Nav.Link as={Link} to="/calculate">
+                  Calculate
+                </Nav.Link>
 
-              <NavDropdown title="User Menu" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
+                <NavDropdown title="User Menu" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
         </Container>
       </Navbar>
     </>
--- client/src/pages/Calculate/index.tsx
+++ client/src/pages/Calculate/index.tsx
@@ -1,11 +1,64 @@
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import LoadPlan from "../../components/LoadPlan";
 
 const Calculate = () => {
   return (
-    <>
-      <div>Calculate</div>
+    <div className="steel-gray-back ">
+      <Container className="pt-5">
+        <Form>
+          <Row>
+            <Col xs={12} md={6} className="mb-4">
+              <h3 className="text-center">Container Dimensions</h3>
+              <Form.Control
+                className="mb-2"
+                type="number"
+                placeholder="Container Width"
+                required
+              />
+              <Form.Control
+                className="mb-2"
+                type="number"
+                placeholder="Container Length"
+                required
+              />
+              <Form.Control
+                className="mb-2"
+                type="number"
+                placeholder="Container Height"
+                required
+              />
+            </Col>
+            <Col xs={12} md={6}>
+              <h3 className="text-center">Box Dimensions</h3>
+              <Form.Control
+                className="mb-2"
+                type="number"
+                placeholder="Box Width"
+                required
+              />
+              <Form.Control
+                className="mb-2"
+                type="number"
+                placeholder="Box Length"
+                required
+              />
+              <Form.Control
+                className="mb-2"
+                type="number"
+                placeholder="Box Height"
+                required
+              />
+            </Col>
+          </Row>
+          <div className="d-flex justify-content-center align-items-center my-3">
+            <Button type="submit" className="cargo-green-btn " color="black">
+              Calculate
+            </Button>
+          </div>
+        </Form>
+      </Container>
       <LoadPlan />
-    </>
+    </div>
   );
 };
 
