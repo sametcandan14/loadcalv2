ba1a744cc799e67b6de16a0bb63c5df7afb06ae0
Samet Candan <c._samet@hotmail.com>
2024-09-03 00:20:43 +0300

    form

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,10 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <img className="logo" src="/uzbo_global.svg" alt="" />
+      <p>2024 uzbo, Tüm Hakları Saklıdır.</p>
+    </div>
+  );
 };
 
 export default Footer;
--- client/src/pages/Calculate/index.tsx
+++ client/src/pages/Calculate/index.tsx
@@ -1,31 +1,64 @@
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import LoadPlan from "../../components/LoadPlan";
+import { FormEvent, useState } from "react";
 
 const Calculate = () => {
+  type valuesT = {
+    contWidth?: number | undefined;
+    contLength?: number | undefined;
+    contHeight?: number | undefined;
+    boxWidth?: number | undefined;
+    boxLength?: number | undefined;
+    boxHeight?: number | undefined;
+  };
+
+  const [values, setValues] = useState<valuesT>();
+
+  const handleChange = (e: FormEvent) => {
+    setValues({
+      ...values,
+      [(e.target as HTMLInputElement).name]: (e.target as HTMLInputElement)
+        .value,
+    });
+
+    console.log(values);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    console.log(values);
+  };
   return (
     <div className="steel-gray-back ">
       <Container className="pt-5">
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Row>
             <Col xs={12} md={6} className="mb-4">
               <h3 className="text-center">Container Dimensions</h3>
               <Form.Control
+                name="contWidth"
                 className="mb-2"
                 type="number"
                 placeholder="Container Width"
                 required
+                onChange={handleChange}
               />
               <Form.Control
                 className="mb-2"
                 type="number"
                 placeholder="Container Length"
+                name="contLength"
                 required
+                onChange={handleChange}
               />
               <Form.Control
                 className="mb-2"
                 type="number"
                 placeholder="Container Height"
+                name="contHeight"
                 required
+                onChange={handleChange}
               />
             </Col>
             <Col xs={12} md={6}>
@@ -34,19 +67,25 @@ const Calculate = () => {
                 className="mb-2"
                 type="number"
                 placeholder="Box Width"
+                name="boxWidth"
                 required
+                onChange={handleChange}
               />
               <Form.Control
                 className="mb-2"
                 type="number"
                 placeholder="Box Length"
+                name="boxLength"
                 required
+                onChange={handleChange}
               />
               <Form.Control
                 className="mb-2"
                 type="number"
                 placeholder="Box Height"
+                name="boxHeight"
                 required
+                onChange={handleChange}
               />
             </Col>
           </Row>
