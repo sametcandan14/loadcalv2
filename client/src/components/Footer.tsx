5d636c0c7aaf2ecd9d90b3f8897e9a3d87b81604
Samet Candan <c._samet@hotmail.com>
2025-10-16 22:44:51 +0300

    plan page

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="footer safety-yellow-back pt-3">
       <img className="logo" src="/uzbo_logo.png" alt="uzbo logo" />
-      <p>2025 uzbo, Tüm Hakları Saklıdır.</p>
+      <p>2025 uzbo, All rights reserved.</p>
     </div>
   );
 };
--- client/src/pages/Plans/index.tsx
+++ client/src/pages/Plans/index.tsx
@@ -1,5 +1,41 @@
 const Plans = () => {
-  return <div>Plans</div>;
+  const satir = 20;
+  const satirsayisi = Array.from({ length: satir });
+  return (
+    <div className="container">
+      <h3 className="text-center mt-2">Total Quantity: 750</h3>
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "2fr 3fr",
+          gap: "10px",
+          alignItems: "center",
+          padding: "5px",
+        }}
+      >
+        <div
+          style={{
+            display: "grid",
+            alignContent: "center",
+            justifyItems: "center",
+            gridTemplateColumns: "1fr 1fr",
+          }}
+        >
+          {satirsayisi.map(() => (
+            <div
+              style={{
+                height: "20px",
+                width: "100%",
+                border: "2px solid black",
+              }}
+            ></div>
+          ))}
+        </div>
+        <div>as</div>
+      </div>
+    </div>
+  );
 };
 
 export default Plans;
